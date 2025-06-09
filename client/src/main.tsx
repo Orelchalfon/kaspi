d55514bb42f5088d49da53dda5702b4c9dc724f1
Orel Chalfon <103241272+Orelchalfon@users.noreply.github.com>
2025-06-09 14:32:18 +0300

    feat: add mobile navigation

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -10,7 +10,9 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <BrowserRouter>
-        <App />
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
       </BrowserRouter>
     </ErrorBoundary>
   </React.StrictMode>
